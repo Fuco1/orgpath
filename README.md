62a01ca6209973e6d8d48a8f168dffe06e9901c6
Matus Goljer <dota.keys@gmail.com>
2013-12-12 18:41:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# org-xpath
+
+An xpath for org!
+
+The code here is wip garbage.  The code will be functional with org when there's a proper package header.  However, if you want to hack on this and patch stuff, feel free to do so.
---
+++ org-xpath.el
@@ -0,0 +1,56 @@
+(defun orgpath-get (&optional query)
+  (-when-let (nodes (cddr (org-element-parse-buffer 'headline)))
+
+    )
+  )
+
+;; /foo/bar/baz -> split into (foo bar baz), turn that into regexps, search for such header hierarchy
+;; /foo//baz -> // means any depth
+;; foo/bar -> bar under foo but at any depth
+;; foo//bar -> bar any depth under foo at any depth
+
+(setq my-struct '(("foo" ("bar" ("baz")) ("brum" ("quux")) ("klask" ("brum"))) ("boo") ("zoo" ("tralala")) ("foo" ("bar" ("baz")))))
+(setq my-struct '(("foo" ("bar" ("baz") ("qux")))))
+
+(defun orgpath-filter (query struct)
+  (cond
+   ((null query) struct)
+   (t (let* ((current-query-raw (car query))
+             (current-query-re (save-match-data (string-match "/*\\(.*?\\)\\'" current-query-raw) (match-string 1 current-query-raw)))
+             (next-level (-keep (lambda (it)
+                                  (orgpath-match query
+                                                 current-query-raw
+                                                 current-query-re
+                                                 it))
+                                struct)))
+        (-keep
+         (lambda (str) (if (car str)
+                           (--when-let (orgpath-filter (car str) (cddr str)) (cons (cadr str) it))
+                         (cdr str)))
+         next-level)))))
+
+(defun orgpath-match (query query-raw query-re struct)
+  (cond
+   ((s-starts-with-p "//" query-raw)
+    (if (string-match-p query-re (car struct))
+        (cons (cdr query) struct)
+      (cons query struct)))
+   ((s-starts-with-p "/" query-raw)
+    (when (string-match-p query-re (car struct))
+      (cons (cdr query) struct)))))
+
+(defun -map-filter (pred fn list)
+  (-map fn (-filter pred list)))
+
+(defmacro --map-filter (pred-form fn-form list)
+  `(--map ,fn-form (--filter ,pred-form ,list)))
+
+(defun orgpath-split (string)
+  (save-match-data
+    (let (re)
+      (while (string-match "\\(/?.+?\\)\\(?:/\\|\\'\\)" string)
+        (push (match-string 1 string) re)
+        (setq string (substring string (match-end 1))))
+      (reverse re))))
+
+(--map-filter (evenp it) (+ 1 it) '(1 2 3 4 5))
